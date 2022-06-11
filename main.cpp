0427047c0c4210ccad08362f2263e9f92fc30d7d
yhamano1965 <yhamano1965@gmail.com>
2022-06-11 14:46:44 +0900

    P126_Sample4.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,26 @@
-#include <iostream>	
+#include <iostream>		
 using namespace std;
 
 int main()
 {
+	int res;
+
+	cout << "整数を入力してください。\n";
+
+	cin >> res;
+
+	if (res == 1)
+	{
+		cout << "1が入力されました。\n";
+	}
+	else if (res == 2)
+	{
+		cout << "2が入力されました。\n";
+	}
+	else
+	{
+		cout << "1か2を入力してください。\n";
+	}
+
 	return 0;
 }
