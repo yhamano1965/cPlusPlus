c10865e058386f8c207cf5fc1d8465adbac25cf9
yhamano1965 <yhamano1965@gmail.com>
2022-06-11 14:48:08 +0900

    P130_Sample5.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,26 @@
-#include <iostream>	
+#include <iostream>			
 using namespace std;
 
 int main()
 {
+	int res;
+
+	cout << "整数を入力してください。\n";
+
+	cin >> res;
+
+	switch (res)
+	{
+	case 1:
+		cout << "1が入力されました。\n";
+		break;
+	case 2:
+		cout << "2が入力されました。\n";
+		break;
+	default:
+		cout << "1か2を入力してください。\n";
+		break;
+	}
+
 	return 0;
 }
