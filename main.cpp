96df1aa439b5fa5facde1639f9983f333bbfd240
yhamano1965 <yhamano1965@gmail.com>
2022-06-11 14:40:18 +0900

    P88_Sample7.cpp

--- main.cpp
+++ main.cpp
@@ -3,5 +3,12 @@ using namespace std;
 
 int main()
 {
+	cout << "short int型のサイズは " << sizeof(short int) << " バイトです。\n";
+	cout << "int型のサイズは " << sizeof(int) << " バイトです。\n";
+	cout << "long int型のサイズは " << sizeof(long int) << " バイトです。\n";
+	cout << "float型のサイズは " << sizeof(float) << " バイトです。\n";
+	cout << "double型のサイズは " << sizeof(double) << " バイトです。\n";
+	cout << "long double型のサイズは " << sizeof(long double) << " バイトです。\n";
+
 	return 0;
 }
