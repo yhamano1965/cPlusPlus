7516ea19c7e4f2e5465820f1db2b22c0a94f1c93
yhamano1965 <yhamano1965@gmail.com>
2022-06-11 14:50:11 +0900

    P147_Sample2.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
-#include <iostream>	
+#include <iostream>		
 using namespace std;
 
 int main()
 {
+	for (int i = 1; i <= 5; i++)
+	{
+		cout << i << " ”Ô–Ú‚ÌŒJ‚è•Ô‚µ‚Å‚·B\n";
+	}
+
+	cout << "ŒJ‚è•Ô‚µ‚ªI‚í‚è‚Ü‚µ‚½B\n";
+
 	return 0;
 }
