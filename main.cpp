79017a7f8124c7976f942177d1969c32b252014d
yhamano1965 <yhamano1965@gmail.com>
2022-06-11 15:31:04 +0900

    P208_Sample11.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,22 @@
 #include <iostream>	
 using namespace std;
 
+void buy(int x = 10);
+
 int main()
 {
+	cout << "1‰ñ–Ú‚Í100–œ‰~‚Åw“ü‚µ‚Ü‚·B\n";
+	buy(100);
+	
+	cout << "\n";
+
+	cout << "2‰ñ–Ú‚ÍƒfƒtƒHƒ‹ƒg‹àŠz‚Åw“ü‚µ‚Ü‚·B\n";
+	buy();
+
 	return 0;
+}
+
+void buy(int x)
+{
+	cout << x << "–œ‰~‚ÌŽÔ‚ð”ƒ‚¢‚Ü‚µ‚½B\n";
 }
