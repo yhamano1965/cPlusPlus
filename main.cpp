6fc371483b4e3dc0a9df616dfa0cbc3ba2ef6e31
yhamano1965 <yhamano1965@gmail.com>
2022-06-12 14:36:45 +0900

    P230_Sample3.cpp

--- main.cpp
+++ main.cpp
@@ -3,5 +3,16 @@ using namespace std;
 
 int main()
 {
+	int a;
+	int* pA;
+
+	a = 5;
+	pA = &a;
+
+	cout << "変数aの値は " << a << " です。\n";
+	cout << "変数aのアドレスは " << &a << " です。\n";
+	cout << "ポインタpAの値は " << pA << " です。\n";
+	cout << "*pAの値は " << *pA << " です。\n";
+
 	return 0;
 }
