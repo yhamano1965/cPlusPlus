97804723289f3f0d3ce8aa49527f631e74830132
yhamano1965 <yhamano1965@gmail.com>
2022-06-12 15:33:17 +0900

    P234_Sample4.cpp

--- main.cpp
+++ main.cpp
@@ -3,16 +3,23 @@ using namespace std;
 
 int main()
 {
-	int a;
+	int a = 5;
+	int b = 10;
 	int* pA;
 
-	a = 5;
 	pA = &a;
 
 	cout << "変数aの値は " << a << " です。\n";
 	cout << "変数aのアドレスは " << &a << " です。\n";
 	cout << "ポインタpAの値は " << pA << " です。\n";
-	cout << "*pAの値は " << *pA << " です。\n";
+	cout << "*pAの値は " << *pA << " です。\n\n";
+
+	pA = &b;
+
+	cout << "変数bの値は " << b << " です。\n";
+	cout << "変数bのアドレスは " << &b << " です。\n";
+	cout << "ポインタpAの値は " << pA << " です。\n";
+	cout << "*pAの値は " << *pA << " です。\n\n";
 
 	return 0;
 }
