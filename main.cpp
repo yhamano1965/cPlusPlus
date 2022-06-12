11370c44d9a42d3e1979b809a16d59ef30cf82b5
yhamano1965 <yhamano1965@gmail.com>
2022-06-12 16:02:13 +0900

    P247_Sample8.cpp

--- main.cpp
+++ main.cpp
@@ -3,5 +3,22 @@ using namespace std;
 
 int main()
 {
+	int a = 5;
+	int& rA = a;
+
+	cout << "変数aの値は " << a << " です。\n";
+	cout << "参照rAの値は " << rA << " です。\n";
+	cout << "変数aのアドレスは " << &a << " です。\n";
+	cout << "参照rAの値は " << &rA << " です。\n\n";
+
+	rA = 50;
+
+	cout << "\n参照rAに50を代入しました。\n\n";
+
+	cout << "変数aの値は " << a << " です。\n";
+	cout << "参照rAの値は " << rA << " です。\n";
+	cout << "変数aのアドレスは " << &a << " です。\n";
+	cout << "参照rAの値は " << &rA << " です。\n\n";
+
 	return 0;
 }
