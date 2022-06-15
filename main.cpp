044e283f660b1a397f31f00ce8cd514cf3fbd193
yhamano1965 <yhamano1965@gmail.com>
2022-06-15 14:05:55 +0900

    P265_Sample1.cpp-1

--- main.cpp
+++ main.cpp
@@ -3,13 +3,7 @@ using namespace std;
 
 int main()
 {
-	int test[5];
-
-	test[0] = 80;
-	test[1] = 60;
-	test[2] = 22;
-	test[3] = 50;
-	test[4] = 75;
+	int test[5] = { 80, 60, 22, 50, 75 };
 
 	for (int i = 0; i < 5; i++)
 	{
