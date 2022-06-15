ca0bfb32c983eaf182815bc8bd2f84991ec6ae44
yhamano1965 <yhamano1965@gmail.com>
2022-06-15 14:20:38 +0900

    P269_Sample3.cpp

--- main.cpp
+++ main.cpp
@@ -3,5 +3,33 @@ using namespace std;
 
 int main()
 {
+	const int num = 5;
+	int test[num];
+
+	cout << num << " 人の点数を入力してください。\n";
+	for (int i = 0; i < num; i++)
+	{
+		cin >> test[i];
+	}
+
+	for (int s = 0; s < num - 1; s++)
+	{
+		for (int t = s + 1; t < num; t++)
+		{
+			if (test[t] > test[s])
+			{
+				int temp = test[t];
+				test[t] = test[s];
+				test[s] = temp;
+			}
+		}
+	}
+
+
+	for (int j = 0; j < num; j++)
+	{
+		cout << j + 1 << " 番の点数は " << test[j] << " です。\n";
+	}
+
 	return 0;
 }
