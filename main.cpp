c3d33929bdb1de9e7c12da5137bfd2e33602df88
yhamano1965 <yhamano1965@gmail.com>
2022-06-15 14:29:17 +0900

    P278_Sample6.cpp

--- main.cpp
+++ main.cpp
@@ -8,7 +8,12 @@ int main()
 	cout << "test[0]の値は " << test[0] << " です。\n";
 	cout << "test[0]のアドレスは " << &test[0] << " です。\n";
 	cout << "testの値は " << test << " です。\n";
-	cout << "*testの値は " << *test << " です。\n";
+	cout << "*testの値は " << *test << " です。\n\n";
+
+	cout << "test[1]の値は " << test[1] << " です。\n";
+	cout << "test[1]のアドレスは " << &test[1] << " です。\n";
+	cout << "test+1の値は " << test+1 << " です。\n";
+	cout << "*(test+1)の値は " << *(test+1) << " です。\n\n";
 
 	return 0;
 }
