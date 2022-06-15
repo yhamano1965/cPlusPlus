1d2ad14a39af4edf307b2e7356d0a98df9796d60
yhamano1965 <yhamano1965@gmail.com>
2022-06-15 14:13:38 +0900

    P267_Sample2.cpp

--- main.cpp
+++ main.cpp
@@ -3,5 +3,19 @@ using namespace std;
 
 int main()
 {
+	const int num = 5;
+	int test[num];
+
+	cout << num << " 人の点数を入力してください。\n";
+	for (int i = 0; i < num; i++)
+	{
+		cin >> test[i];
+	}
+
+	for (int j = 0; j < num; j++)
+	{
+		cout << j+1 << " 番の点数は " << test[j] << " です。\n";
+	}
+
 	return 0;
 }
