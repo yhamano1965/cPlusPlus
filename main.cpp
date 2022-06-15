86246680ed6f74497c3ccd49fe696e78331d5aa2
yhamano1965 <yhamano1965@gmail.com>
2022-06-15 14:26:24 +0900

    P276_Sample5.cpp

--- main.cpp
+++ main.cpp
@@ -3,5 +3,12 @@ using namespace std;
 
 int main()
 {
+	int test[] = { 80, 60, 55, 22, 75 };
+
+	cout << "test[0]の値は " << test[0] << " です。\n";
+	cout << "test[0]のアドレスは " << &test[0] << " です。\n";
+	cout << "testの値は " << test << " です。\n";
+	cout << "*testの値は " << *test << " です。\n";
+
 	return 0;
 }
