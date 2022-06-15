c878af0ee7ed7c1acbc707bedc9561145f45975a
yhamano1965 <yhamano1965@gmail.com>
2022-06-15 14:03:39 +0900

    P263_Sample1.cpp

--- main.cpp
+++ main.cpp
@@ -3,5 +3,18 @@ using namespace std;
 
 int main()
 {
+	int test[5];
+
+	test[0] = 80;
+	test[1] = 60;
+	test[2] = 22;
+	test[3] = 50;
+	test[4] = 75;
+
+	for (int i = 0; i < 5; i++)
+	{
+		cout << i + 1 << " ”Ô‚Ì“_”‚Í " << test[i] << " ‚Å‚·B\n";
+	}
+
 	return 0;
 }
