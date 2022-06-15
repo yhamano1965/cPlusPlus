1362afe61a9bef9c68f900e3e549ccccaed9c208
yhamano1965 <yhamano1965@gmail.com>
2022-06-15 14:41:59 +0900

    P281_Sample7.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,34 @@
 #include <iostream>	
 using namespace std;
 
+const int num = 5;
+
+double avg(int t[]);
+
 int main()
 {
+	int test[num];
+
+	cout << num << " 人の点数を入力してください。\n";
+	for (int i = 0; i < num; i++)
+	{
+		cin >> test[i];
+	}
+
+	double ans = avg(test);
+	cout << num << "人の平均点は " << ans << " 点です。\n";
+
 	return 0;
+}
+
+double avg(int t[])
+{
+	double sum = 0;
+
+	for (int i = 0; i < num; i++)
+	{
+		sum += t[i];
+	}
+
+	return sum / num;
 }
