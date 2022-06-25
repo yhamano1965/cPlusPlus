be59d7c3c83c8c97bb60630d7f40be6a423f5669
yhamano1965 <yhamano1965@gmail.com>
2022-06-25 13:52:43 +0900

    P303_Sample1.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,28 @@
 #include <iostream>	
 using namespace std;
 
+void func();
+
+int a = 0;
+
 int main()
 {
+	int b = 1;
+
+	cout << "mainŠÖ”‚Å‚Í\n";
+	cout << "a = " << a << "\n";
+	cout << "b = " << b << "\n";
+
+	func();
+
 	return 0;
+}
+
+void func()
+{
+	int c = 2;
+
+	cout << "funcŠÖ”‚Å‚Í\n";
+	cout << "a = " << a << "\n";
+	cout << "c = " << c << "\n";
 }
