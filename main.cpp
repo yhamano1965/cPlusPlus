18dbf6e08e96471de827a853229b4eca562b672e
yhamano1965 <yhamano1965@gmail.com>
2022-06-25 14:01:38 +0900

    P311_Sample2.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,31 @@
 #include <iostream>	
 using namespace std;
 
+void func();
+
+int a = 0;
+
 int main()
 {
+	for (int i = 0; i < 5; i++)
+	{
+		cout << i << "‰ñ–Ú\n";
+		func();
+	}
+
 	return 0;
+}
+
+void func()
+{
+	int b = 0;
+	static int c = 0;
+
+	cout << "a = " << a << "\n";
+	cout << "b = " << b << "\n";
+	cout << "c = " << c << "\n\n";
+
+	a++;
+	b++;
+	c++;
 }
