4ee9b9a4badfde00cf444be820921b57244d9077
yhamano1965 <yhamano1965@gmail.com>
2022-06-25 14:24:57 +0900

    P321_Sample4.cpp

--- main.cpp
+++ main.cpp
@@ -3,5 +3,26 @@ using namespace std;
 
 int main()
 {
+	int num;
+	int* pT;
+
+	cout << "何人のテストの点数を入力しますか？\n";
+	cin >> num;
+
+	pT = new int[num];
+	cout << num << "人の点数を入力してください。\n";
+
+	for (int i = 0; i < num; i++)
+	{
+		cin >> pT[i];
+	}
+
+	for (int j = 0; j < num; j++)
+	{
+		cout << j+1 << "番目の点数は" << pT[j] << "です\n";
+	}
+
+	delete pT;
+
 	return 0;
 }
