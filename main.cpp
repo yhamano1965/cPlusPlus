ad7c6f36eb407fb91b83ae6a0e230926e8906439
yhamano1965 <yhamano1965@gmail.com>
2022-06-25 14:36:01 +0900

    P325_Sample5.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>	
+#include "myfunc.h"
 using namespace std;
 
 int main()
 {
+	int num1, num2, ans;
+
+	cout << "‚P”Ô–Ú‚Ì®”‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢B\n";
+	cin >> num1;
+
+	cout << "‚Q”Ô–Ú‚Ì®”‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢B\n";
+	cin >> num2;
+
+	ans = max(num1, num2);
+
+	cout << "Å‘å’l‚Í" << ans << "‚Å‚·B\n";
+
 	return 0;
 }
---
+++ myfunc.cpp
@@ -0,0 +1,15 @@
+#include <iostream>	
+#include "myfunc.h"
+using namespace std;
+
+int max(int x, int y)
+{
+	if (x > y)
+	{
+		return x;
+	}
+	else
+	{
+		return y;
+	}
+}
