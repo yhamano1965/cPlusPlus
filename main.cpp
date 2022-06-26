f1fc8d9138ce2642b0daab6b89b01771be957b11
yhamano1965 <yhamano1965@gmail.com>
2022-06-26 13:43:34 +0900

    P342_Sample2.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>	
+#include "myfunc.h"
 using namespace std;
 
 int main()
 {
+	Car car1;
+
+	cout << "ナンバーは？\n";
+	cin >> car1.num;
+
+	cout << "ガソリン量は？\n";
+	cin >> car1.gas;
+
+	cout << "車のナンバーは" << car1.num << "：ガソリン量は" << car1.gas << "です。\n";
+
 	return 0;
 }
---
+++ myfunc.h
@@ -0,0 +1,5 @@
+struct Car
+{
+	int num;
+	double gas;
+};
