212cbd59a33f87053ce45d77c9463a012cfa4ac7
yhamano1965 <yhamano1965@gmail.com>
2022-06-27 16:51:01 +0900

    P396_Sample3.cpp

--- main.cpp
+++ main.cpp
@@ -4,11 +4,12 @@ using namespace std;
 
 int main()
 {
-	Car car1;
-	car1.show();
-
-	Car car2(1234, 20.5);
-	car2.show();
+	Car mycars[3] =
+	{
+		Car(),
+		Car(1234, 20.5),
+		Car(4567, 52.2)
+	};
 
 	return 0;
 }
