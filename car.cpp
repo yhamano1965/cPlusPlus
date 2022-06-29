1c4efa6be139cb12cde0b239705c5afc7fa3548e
yhamano1965 <yhamano1965@gmail.com>
2022-06-29 21:04:17 +0900

    P427_Sample3.cpp

--- car.cpp
+++ car.cpp
@@ -1,4 +1,4 @@
-#include <iostream>	
+#include <iostream>
 #include "car.h"
 #include "rccar.h"
 using namespace std;
--- car.h
+++ car.h
@@ -1,11 +1,11 @@
 class Car
 {
-	protected:
-		int num;
-		double gas;
-	public:
-		Car();
-		Car(int n, double g);
-		void setCar(int n, double g);
-		void show();
+protected:
+	int num;
+	double gas;
+public:
+	Car();
+	Car(int n, double g);
+	void setCar(int n, double g);
+	void show();
 };
