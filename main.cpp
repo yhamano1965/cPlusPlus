f8199722838bd8461d6499d00168f87c46a9a7b4
yhamano1965 <yhamano1965@gmail.com>
2022-06-30 17:12:03 +0900

    P430_Sample4.cpp

--- main.cpp
+++ main.cpp
@@ -5,13 +5,21 @@ using namespace std;
 
 int main()
 {
-	RacingCar rccar1;
-	rccar1.setCar(1234, 20.5);
-	rccar1.setCourse(5);
-	rccar1.show();
+	Car* pCars[2];
 
-	RacingCar rccar2(4567, 50.2, 10);
-	rccar2.show();
+	Car car1;
+	RacingCar rccar1;
+
+	pCars[0] = &car1;
+	pCars[0]->setCar(1234, 20.5);
+
+	pCars[1] = &rccar1;
+	pCars[1]->setCar(4567, 30.5);
+
+	for (int i = 0; i < 2; i++)
+	{
+		pCars[i]->show();
+	}
 
 	return 0;
 }
